c49158a386bfa1c3ec8ef54a45c9702d577975a3
PONSHANKAR03 <ponshankarbalu1998@gmail.com>
2025-03-23 12:21:57 +0530

    datatype

---
+++ src/org/data/Data.java
@@ -0,0 +1,21 @@
+package org.data;
+
+public class Data {
+
+	public static void main(String[] args) {
+		
+		byte age =26;
+		short income = 15000;
+		int accNo = 234567890;
+		long mobileNo = 7339423846l;
+		float percentage = 99.123456f;
+		double point = 87.6543212345;
+		String name = "naveen";
+		String hometown = "naveen native is manargudi";
+		boolean status = true;
+		boolean inform = false;
+		System.out.println("Age is "+age+ "\n" +"income is "+income+"\n"+"Account no is "+accNo+"\n"+"Mobile number is "+mobileNo+"\n"+"percentage is "+percentage+"\n"+"annual performance is "+point+"\n"+"brother name is "+name+"\n"+"Hometown of "+hometown+"\n"+		"he is good boy "+status+"\n"+"statement is "+inform);
+	
+			}
+
+}
---
+++ src/org/data/Employee.java
@@ -0,0 +1,32 @@
+package org.data;
+
+public class Employee {
+public static void main(String[] args) {
+	byte age=23;
+	System.out.println("age is "+age);
+	short salary=10000;
+	System.out.println("salary is "+salary);
+	int accNo=123456789;
+	System.out.println("account num is "+accNo);
+	long mobileNo=9876543210l;
+	System.out.println("mobile num is "+mobileNo);
+	float percentage=98.98765f;
+	System.out.println("percentage is "+percentage);
+	double turnover=65.987654321098;
+	System.out.println("annual turnover is "+turnover);
+	String companyName="ponshankar";
+	System.out.println("owner of company is "+companyName);
+	
+	char gender='M';
+	char child='2';
+	System.out.println("gender is "+gender+"\n"+"total no child is "+child);
+	
+	boolean good=true;
+	boolean bad=false;
+	System.out.println("he is good "+good+"\n"+"he is not bad"+bad);
+	
+	
+}
+	
+	
+}
