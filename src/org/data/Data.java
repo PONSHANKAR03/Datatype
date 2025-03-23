447337c772750f936c831cba19bff22addab01a4
PONSHANKAR03 <ponshankarbalu1998@gmail.com>
2025-03-23 12:37:30 +0530

    tester

--- src/org/data/Data.java
+++ src/org/data/Data.java
@@ -3,8 +3,8 @@ package org.data;
 public class Data {
 
 	public static void main(String[] args) {
-		
-		byte age =26;
+
+		byte age = 26;
 		short income = 15000;
 		int accNo = 234567890;
 		long mobileNo = 7339423846l;
@@ -14,8 +14,13 @@ public class Data {
 		String hometown = "naveen native is manargudi";
 		boolean status = true;
 		boolean inform = false;
-		System.out.println("Age is "+age+ "\n" +"income is "+income+"\n"+"Account no is "+accNo+"\n"+"Mobile number is "+mobileNo+"\n"+"percentage is "+percentage+"\n"+"annual performance is "+point+"\n"+"brother name is "+name+"\n"+"Hometown of "+hometown+"\n"+		"he is good boy "+status+"\n"+"statement is "+inform);
-	
-			}
+		System.out.println("Age is " + age + "\n" + "income is " + income + "\n" + "Account no is " + accNo + "\n"
+				+ "Mobile number is " + mobileNo + "\n" + "percentage is " + percentage + "\n"
+				+ "annual performance is " + point + "\n" + "brother name is " + name + "\n" + "Hometown of " + hometown
+				+ "\n" + "he is good boy " + status + "\n" + "statement is " + inform);
+
+		System.out.println("shankar");
+
+	}
 
 }
